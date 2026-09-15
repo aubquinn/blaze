a079e2d9a92683d5c9538bc702a8ff7deb229ced
Aubrey Quinn <aubreyfo@gmail.com>
2026-09-15 15:15:51 +0100

    Added design system provider to Storybook config

--- client/.storybook/preview.tsx
+++ client/.storybook/preview.tsx
@@ -1,5 +1,11 @@
 import type { Preview } from '@storybook/react-vite'
 
+import { Theme } from "@astryxdesign/core/theme";
+import { neutralTheme } from "@astryxdesign/theme-neutral/built";
+
+import "@astryxdesign/core/reset.css";
+import "@astryxdesign/core/astryx.css";
+
 const preview: Preview = {
   parameters: {
     controls: {
@@ -9,6 +15,13 @@ const preview: Preview = {
       },
     },
   },
+    decorators: [
+    (Story) => (
+      <Theme theme={neutralTheme} mode="system">
+        <Story />
+      </Theme>
+    ),
+  ],
 };
 
 export default preview;
