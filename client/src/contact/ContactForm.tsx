8d084671ddc11658a11a0c770e1873682b3b4c4a
Aubrey Quinn <aubreyfo@gmail.com>
2026-09-15 15:08:27 +0100

    Added the boilerplate code for the contact form

---
+++ client/src/contact/ContactForm.tsx
@@ -0,0 +1,22 @@
+
+import { FormLayout } from "@astryxdesign/core/FormLayout";
+import { TextInput } from "@astryxdesign/core/TextInput";
+import { TextArea } from '@astryxdesign/core/TextArea'
+import { Button } from "@astryxdesign/core/Button";
+
+function ContactForm() {
+
+  return (
+    <>
+      <FormLayout>
+        <TextInput label="Full Name" value={''} />
+        <TextInput label="Email Address" value={''} />
+        <TextInput label="Subject" value={''} />
+        <TextArea label="Message" value={''} />
+        <Button label="Submit" />
+      </FormLayout>
+    </>
+  )
+}
+
+export default ContactForm
